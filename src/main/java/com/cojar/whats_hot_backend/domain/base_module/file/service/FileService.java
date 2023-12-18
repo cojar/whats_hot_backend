7a7bec5c2ab69bf3333f7ac3c1c2eef9003540a8
pintor.dev <pintor.dev@gmail.com>
2023-12-18 20:26:37 +0900

    F-00-00-01 TC 작성

--- src/main/java/com/cojar/whats_hot_backend/domain/base_module/file/service/FileService.java
+++ src/main/java/com/cojar/whats_hot_backend/domain/base_module/file/service/FileService.java
@@ -4,9 +4,16 @@ import com.cojar.whats_hot_backend.domain.base_module.file.entity.SaveFile;
 import com.cojar.whats_hot_backend.domain.base_module.file.repository.FileRepository;
 import com.cojar.whats_hot_backend.domain.review_module.review.entity.Review;
 import com.cojar.whats_hot_backend.domain.spot_module.spot.entity.Spot;
+import com.cojar.whats_hot_backend.global.response.ResData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -35,5 +42,48 @@ public class FileService {
         this.fileRepository.save(saveFile);
 
         return saveFile;
+    }
+
+    public ResData validate(MultipartFile file) {
+
+        Map<String, Object> fileBits = this.getFileBits(file);
+        Errors errors = new BeanPropertyBindingResult(file, "file");
+
+        if (!fileBits.get("type").equals("image")) {
+
+            errors.rejectValue("contentType", "not allowed", "content type is not allowed");
+
+            return ResData.of(
+                    HttpStatus.BAD_REQUEST,
+                    "F-00-00-01",
+                    "이미지 형식만 업로드할 수 있습니다",
+                    errors
+            );
+        }
+
+        return null;
+    }
+
+    private Map<String, Object> getFileBits(MultipartFile file) {
+
+        String[] fileBits = file.getContentType().split("/");
+        String type = fileBits[0];
+        String ext;
+
+        if (fileBits.length == 2) {
+            switch (fileBits[1].toLowerCase()) {
+                case "jpg", "jpeg" -> ext = "jpg";
+                case "png" -> ext = "png";
+                default -> ext = "etc";
+            }
+        } else {
+            ext = "etc";
+        }
+
+        return Map.of(
+                "type", type,
+                "ext", ext,
+                "size", file.getSize()
+        );
     }
 }
---
+++ src/test/java/com/cojar/whats_hot_backend/domain/base_module/file/service/FileServiceTest.java
@@ -0,0 +1,64 @@
+package com.cojar.whats_hot_backend.domain.base_module.file.service;
+
+import com.cojar.whats_hot_backend.global.response.ResData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FileServiceTest {
+
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Test
+    @DisplayName("validate fail; content type, F-00-00-01")
+    public void validate_Fail_ContentType() throws Exception {
+
+        // given
+        String name = "test";
+        String ext = "png";
+        Resource resource = resourceLoader.getResource("classpath:/static/image/%s.%s".formatted(name, ext));
+        MockMultipartFile _file = new MockMultipartFile(
+                "file",
+                "%s.%s".formatted(name, ext),
+                "aaa/%s".formatted(ext),
+                resource.getInputStream()
+        );
+
+        // when
+        ResData resData = this.fileService.validate(_file);
+
+        // then
+        assertThat(resData).isNotNull();
+        assertThat(resData.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(resData.isSuccess()).isFalse();
+        assertThat(resData.getCode()).isEqualTo("F-00-00-01");
+        assertThat(resData.getMessage()).isEqualTo("이미지 형식만 업로드할 수 있습니다");
+        assertThat(resData.getData()).isInstanceOf(Errors.class);
+
+        Errors errors = (Errors) resData.getData();
+        FieldError fieldError = errors.getFieldErrors().get(0);
+
+        assertThat(fieldError.getField()).isEqualTo("contentType");
+        assertThat(fieldError.getObjectName()).isEqualTo("file");
+        assertThat(fieldError.getCode()).isEqualTo("not allowed");
+        assertThat(fieldError.getDefaultMessage()).isEqualTo("content type is not allowed");
+        assertThat(fieldError.getRejectedValue()).isEqualTo("aaa/%s".formatted(ext));
+    }
+
+}
