f75575f283d053e02b39617655ac08ea5b6018ed
pintor.dev <pintor.dev@gmail.com>
2024-01-09 22:05:23 +0900

    cors 허용 테스트

--- src/main/java/com/cojar/whats_hot_backend/global/util/WebMvcConfig.java
+++ src/main/java/com/cojar/whats_hot_backend/global/util/WebMvcConfig.java
@@ -2,10 +2,13 @@ package com.cojar.whats_hot_backend.global.util;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${file.resource.path}")
@@ -17,5 +20,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
         resourceHandlerRegistry.addResourceHandler(requestPath).addResourceLocations(resourcePath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://whats-hot-frontend.vercel.app")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
     }
 }
