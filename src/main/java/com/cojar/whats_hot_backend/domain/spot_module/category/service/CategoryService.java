60b8a7df3c2b9450524da273f88c9a450ffd4daa
hdYang <gmlehd0201@gmail.com>
2024-01-12 18:10:06 +0900

    category create 1차
    카테고리 depth 관련
    depth가 1차 이거나, 4차 이상 만들지 않음
    root category name이 맛집인 경우 3차까지, 나머지는 2차까지 생성
    
    initConfig 용 create 분리

--- src/main/java/com/cojar/whats_hot_backend/domain/spot_module/category/service/CategoryService.java
+++ src/main/java/com/cojar/whats_hot_backend/domain/spot_module/category/service/CategoryService.java
@@ -2,6 +2,9 @@ package com.cojar.whats_hot_backend.domain.spot_module.category.service;
 
 import com.cojar.whats_hot_backend.domain.spot_module.category.entity.Category;
 import com.cojar.whats_hot_backend.domain.spot_module.category.repository.CategoryRepository;
+import com.cojar.whats_hot_backend.global.errors.exception.ApiResponseException;
+import com.cojar.whats_hot_backend.global.response.ResCode;
+import com.cojar.whats_hot_backend.global.response.ResData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,41 @@ public class CategoryService {
 
     @Transactional
     public Category create(String name, Integer depth, Long parentId) {
+        // 1차 카테고리는 만들지 않고 4차부터도 만들지 않음
+        if(depth == 1 || depth >= 4){
+            throw new ApiResponseException(
+                    ResData.of(
+                            ResCode.F_05_01_01
+                    )
+            );
+        }
+
+        Category parent = this.categoryRepository.findById(parentId == null ? -1 : parentId)
+                .orElse(null);
+
+        // 최상단 카테고리가 맛집일 경우 depth 3, 나머지는 2까지 생성 가능
+        if( depth == 3 && !parent.getRootName().equals("맛집")){
+            throw new ApiResponseException(
+                    ResData.of(
+                            ResCode.F_05_01_01
+                    )
+            );
+        }
+
+        Category category = Category.builder()
+                .name(name)
+                .depth(depth)
+                .parent(parent)
+                .build();
+
+        this.categoryRepository.save(category);
+
+        return category;
+    }
+
+    // InitConfig용
+    @Transactional
+    public Category createForInit(String name, Integer depth, Long parentId) {
 
         Category parent = this.categoryRepository.findById(parentId == null ? -1 : parentId)
                 .orElse(null);
